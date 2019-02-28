738d4739cf8c52a67712ebb3ff93f14bae48e8fb
gq <gq@admin-PC>
2019-02-28 10:45:32 +0800

    mobx未使用装饰器，使用案例

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import './App.css';
-import { observable,action } from 'mobx';
+import {extendObservable } from 'mobx';
 
 class App extends Component {
+  constructor () {
+    super();
+    extendObservable(this, {
+      count:0
+    })
+  }
+  onIncrement = () => {
+    this.count++;
+  }
+  onDecrement = () => {
+    this.count--;
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <span>{ this.count }</span>
+          <button style={ { width: "100px",height:"100px",textAlign:"center",lineHeight:"100px"}} onClick={this.onIncrement}>up</button>
+          <button style={{width: "100px",height:"100px",textAlign:"center",lineHeight:"100px"}} onClick={this.onDecrement}>down</button>
         </header>
       </div>
     );
   }
 }
-class appStore {
-  @observable name = 'gl'
-  @action change(){
-  // this.name='gaoliang'
-  console.log('changed')
-  }
-}
-export default App;
+
+export default observer(App);
